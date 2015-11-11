25aec8d5174ccbeb44c8270b2c00a3e8620f007a
SARA SIMON <smbsimon@gmail.com>
2015-11-11 17:19:14 -0500

    No longer hardcoding in one hospital. The bot now searches for reports from all Vermont hospitals. Still to do: format tweet.

--- index.py
+++ index.py
@@ -11,11 +11,3 @@ def new_tweet():
             new_tweets.append(row)
 
         return new_tweets
-
-
-
-
-# foundation_url = "http://www.dlp.vermont.gov/standard-surveys-hospitals"
-# slug = {'Brattleboro Memorial Hospital': '/brattleboro-acc-poc-folder/', 'Brattleboro Retreat': '/brattleboro-retreat-acc-poc-folder/', 'Central Vermont Medical Center': '/central-vermont-acc-poc-folder/', 'Copley Hospital': '/copley-acc-poc-folder/', 'Fletcher Allen Health Care': '/fletcher-allen-acc-poc-folder/', 'Gifford Memorial Hospital': '/gifford-acc-poc-folder/', 'Grace Cottage Hospital': '/grace-acc-poc-folder/', 'Vermont Psychiatric Care Hospital': '/green-mountain-psychiatric-care-center/', 'Mt. Ascutney Hospital & Health Center': '/mt-ascutney-acc-poc-folder/', 'North Country Hospital & Health Center': '/north-country-acc-poc-folder/', 'Northeastern Vermont Regional Hospital': '/northeastern-acc-poc-folder/', 'Northwestern Medical Center': '/northwestern-acc-poc-folder/', 'Porter Hospital': '/porter-acc-poc-folder/', 'Rutland Regional Medical Center': '/rutland-acc-poc-folder/', 'Southwestern Vermont Medical Center': '/southwestern-acc-poc-folder/', 'Springfield Hospital': '/springfield-acc-poc-folder/', 'Navigation': 'portlet-navigation-tree'}
-#
-# print foundation_url + slug['Brattleboro Memorial Hospital']
--- scrape.py
+++ scrape.py
@@ -1,23 +1,30 @@
 #!/usr/local/bin/python2.7
+
 import csv
 import requests
 from bs4 import BeautifulSoup
 
-brattleboro = 'http://www.dlp.vermont.gov/standard-surveys-hospitals/brattleboro-acc-poc-folder/'
-response = requests.get(brattleboro)
-html = response.content
-soup = BeautifulSoup(html)
-
-reports = soup.find_all('dt')
-list_of_reports = []
-
-for report in reports:
-    new_report = report.a.string
-    list_of_reports.append(str(new_report))
-
-del list_of_reports[-1]
-
+hospital_foundation = 'http://www.dlp.vermont.gov/standard-surveys-hospitals'
+slug = {'Brattleboro Memorial Hospital': '/brattleboro-acc-poc-folder/', 'Brattleboro Retreat': '/brattleboro-retreat-acc-poc-folder/', 'Central Vermont Medical Center': '/central-vermont-acc-poc-folder/', 'Copley Hospital': '/copley-acc-poc-folder/', 'Fletcher Allen Health Care': '/fletcher-allen-acc-poc-folder/', 'Gifford Memorial Hospital': '/gifford-acc-poc-folder/', 'Grace Cottage Hospital': '/grace-acc-poc-folder/', 'Vermont Psychiatric Care Hospital': '/green-mountain-psychiatric-care-center/', 'Mt. Ascutney Hospital & Health Center': '/mt-ascutney-acc-poc-folder/', 'North Country Hospital & Health Center': '/north-country-acc-poc-folder/', 'Northeastern Vermont Regional Hospital': '/northeastern-acc-poc-folder/', 'Northwestern Medical Center': '/northwestern-acc-poc-folder/', 'Porter Hospital': '/porter-acc-poc-folder/', 'Rutland Regional Medical Center': '/rutland-acc-poc-folder/', 'Southwestern Vermont Medical Center': '/southwestern-acc-poc-folder/', 'Springfield Hospital': '/springfield-acc-poc-folder/', 'Navigation': 'portlet-navigation-tree'}
 
 outfile = open("./reports.csv", "wb")
 writer = csv.writer(outfile)
-writer.writerows([list_of_reports])
+
+for key, value in slug.iteritems():
+
+    hospital_url = hospital_foundation + value
+
+    response = requests.get(hospital_url)
+    html = response.content
+    soup = BeautifulSoup(html)
+
+    reports = soup.find_all('dt')
+    list_of_reports = []
+
+    for report in reports:
+        new_report = report.a.string
+        list_of_reports.append(str(new_report))
+
+    del list_of_reports[-1]
+
+    writer.writerows([list_of_reports])
