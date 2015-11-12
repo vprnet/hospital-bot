37291590db91cd50f501fdcf777bb39cd6151323
SARA SIMON <smbsimon@gmail.com>
2015-11-12 17:08:42 -0500

    BOOM, this is working. Time to try it out in the real world. 🎉

--- tweet.py
+++ tweet.py
@@ -15,15 +15,16 @@ with open('./past_tweets.log', 'r') as f:
     for row in reader:
         past_tweets.append(row)
 
-    for row in scraped_tweets:
-        for report_tweet in row:
-            duplicate = [s for s in past_tweets if report_tweet in s]
+    for report_tweet in scraped_tweets:
+        cleaned_tweet = " ".join(report_tweet)
 
-            if duplicate:
-                print "Already been tweeted."
-            else:
-                logging.basicConfig(filename='past_tweets.log', level=logging.INFO)
-                logging.info(report_tweet)
-                # twitter.update_status(status=report_tweet)
-                print report_tweet
-                print "New tweet tweeted."
+        duplicate = [s for s in past_tweets if cleaned_tweet in s]
+
+        if duplicate:
+            print "Already been tweeted."
+        else:
+            logging.basicConfig(filename='past_tweets.log', level=logging.INFO)
+            logging.info(cleaned_tweet)
+            twitter.update_status(status=cleaned_tweet)
+            print cleaned_tweet
+            print "New tweet tweeted."
